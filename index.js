b1d6c5379263c32675d09f3312c5e106dc0c95fb
tauhid102 <tauhidurr102@gmail.com>
2022-01-03 13:08:53 +0600

    connect database

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { MongoClient }=require('mongodb');
+const cors = require('cors');
+const Object = require('mongodb').ObjectId;
+require ('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jfvuq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run(){
+    try{
+        await client.connect();
+        const database = client.db('Argon');
+        const productCollection = database.collection('products');
+
+        //get all products
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+    }
+    finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Server run');
+})
+app.listen(port, () => {
+    console.log('Server run:', port);
+})
+
