9b55f0d33f9c1977561ffba9452d6b20aee47554
tauhid102 <tauhidurr102@gmail.com>
2022-01-03 14:29:07 +0600

    create database

--- index.js
+++ index.js
@@ -20,14 +20,139 @@ async function run(){
         await client.connect();
         const database = client.db('Argon');
         const productCollection = database.collection('products');
+        const purchasedCollection = database.collection('purchased');
+        const usersCollection = database.collection('users');
+        const reviewsCollection = database.collection('reviews');
 
-        //get all products
+        await client.connect();
+        const database = client.db('Argon');
+        const productCollection = database.collection('products');
+        const purchasedCollection = database.collection('purchased');
+        const usersCollection = database.collection('users');
+        const reviewsCollection = database.collection('reviews');
+        //get all service we provide
         app.get('/products', async (req, res) => {
             const cursor = productCollection.find({});
             const products = await cursor.toArray();
             res.send(products);
         });
+        // fetch by id
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) }
+            const result = await productCollection.findOne(query);
+            console.log('load', result);
+            res.send(result);
+        });
+        //delete product
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await serviceCollection.deleteOne(query);
+            res.json(result);
+        });
+        //save order data
+        app.post('/purchased', async (req, res) => {
+            const cursor = req.body;
+            const result = await purchasedCollection.insertOne(cursor);
+            res.json(result);
+        });
+        //find all the order
+        app.get('/purchased/allorder', async (req, res) => {
+            const cursor = purchasedCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+        //find order using email
+        app.get('/purchased', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const cursor = purchasedCollection.find(query);
+            const buy = await cursor.toArray();
+            res.json(buy);
 
+        });
+
+        //cancel order
+        app.delete('/purchased/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await purchasedCollection.deleteOne(query);
+            res.json(result);
+        });
+        //set user in database
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        });
+        //set user in database
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+        //make admin
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email }
+            const updateDoc = { $set: { role: 'admin' } }
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        });
+        //find admin role
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        });
+        //confirmed order
+        app.put('/purchased/:id',async(req,res)=>{
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                  "status": "Shipped"
+                },
+              };
+            const result=await purchasedCollection.updateOne(query,updateDoc);
+            res.json(result);
+        }); 
+        //add product
+        app.post('/services', async (req, res) => {
+            const cursor = req.body;
+            const result = await serviceCollection.insertOne(cursor);
+            res.json(result);
+        });
+        //set review
+        app.post('/reviews', async (req, res) => {
+            const cursor = req.body;
+            const result = await reviewsCollection.insertOne(cursor);
+            res.json(result);
+        });
+        //find all the order
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewsCollection.find({});
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+        //cancel review
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await reviewsCollection.deleteOne(query);
+            res.json(result);
+        });
     }
     finally{
         // await client.close();
